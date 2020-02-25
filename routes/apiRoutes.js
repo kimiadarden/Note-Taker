b8fc05c393b81afac70963e5ce0b4ffcf23fe62e
Kimia Darden <kimiadarden@gmail.com>
2020-02-25 13:16:19 -0500

    adding require to apiRoute

---
+++ routes/apiRoutes.js
@@ -0,0 +1,13 @@
+var noteContents = require("../db/db")
+const util = require("util");
+const fs = require("fs");
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+module.exports= function(app){
+
+
+
+
+    
+}
